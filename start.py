4059848e2a41cedd4fa32a89b1bbae49b1c77d03
Green_박화희 <hhpark@maxst.com>
2024-07-09 18:13:45 +0900

    예약기능 구현 및 1회 실행
    - 예약가능한 자리 조회
    - 이미 예약되어 있으면 종료

--- start.py
+++ start.py
@@ -9,6 +9,8 @@ url = "https://wpilates031.cafe24.com/index.php?mid=yeyak&act=dispYeyakOrder"
 login_data_dir = '../user_data/wpilates031'
 ID = 'id'
 PW = 'pw'
+DATE = '2024-07-09'
+TIME = '2030'
 
 
 options = Options()
@@ -26,7 +28,36 @@ def login(id: str, pw: str):
     input_pw.send_keys(Keys.RETURN)
 
 
+def reserve(date: str, time: str):
+    enables = driver.find_elements(By.CSS_SELECTOR, f'li.li-data[data-vdate="{date}"][data-classtime="{time}"]')
+    print(f"get_enable : {len(enables)}")
+
+    if not enables:
+        return False
+    
+    for enable in enables:
+        if not enable.get_attribute('usedata'):
+            print('already reserved')
+            return True
+
+    # 확인버튼 클릭
+    enables[0].click()
+    print("click!!")
+    return True
+
+
 # main
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 login(ID, PW)
+
+# 다음주 버튼 클릭
+# move_next_week = driver.find_element(By.LINK_TEXT, '다음주').click()
+
+result = reserve(DATE, TIME)
+print(f"result : {result}")
+
+
+
+
+# <li class="li-data " data-id="412" data-unit="3" data-usedate="20240708" data-vw="월" data-vtitle="황톨" data-vdate="2024-07-08" data-vclasstitle="바렐" data-classtime="2030" data-am="오후" data-h="8" data-m="30">&nbsp;</li>
